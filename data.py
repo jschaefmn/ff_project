fc6ca46153ca581e9bebf41802bec1008538f848
jschaefmn <jschaefmn@gmail.com>
2024-08-13 10:17:00 -0500

    worked on scatterplot and heatmaps

--- data.py
+++ data.py
@@ -1,10 +1,14 @@
 import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
+import glob
   
-df = pd.read_csv('data/*.csv')
+data_files = glob.glob('data/*.csv')
+df = pd.concat([pd.read_csv(f) for f in data_files])
 
 
 
-if __name__ == "__main__":
-  main()
+
+
+# if __name__ == "__main__":
+#   main()
